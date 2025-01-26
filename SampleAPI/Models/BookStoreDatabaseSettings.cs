80b8ca90e45e4626c02dc5bf276c1f29d43cfa4a
dkmint <duongminh19055c@gmail.com>
2025-01-26 18:20:05 +0700

    commit lan 1

---
+++ SampleAPI/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleAPI.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = "mongodb://localhost:27017/";
+        public string DatabaseName { get; set; } = "BookStore"!;
+        public string BooksCollectionName { get; set; } = "Books"!;
+    }
+}
